4eea184d581c20baab7dadf291ee1dc08d4f4c22
gitraiwaqas <sunnyraimobile@gmail.com>
2025-06-25 09:25:18 +0500

    add new ans

---
+++ Variables & Data Types/Ans36.js
@@ -0,0 +1,9 @@
+const checkNumber = (value1, value2) => {
+  if (value1 === value2) {
+    console.log("Value and type is same.");
+  } else {
+    console.log("Value and type is not same.");
+  }
+};
+
+checkNumber(34, 34);
